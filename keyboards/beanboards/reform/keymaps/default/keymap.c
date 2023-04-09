5c79fc9a6aa2d57f07733d7f3fbd7dd883ab9287
C44Supra <sebastiaan.swinkels@googlemail.com>
2023-04-09 22:54:14 +0200

    Updated keymap

--- keyboards/beanboards/reform/keymaps/default/keymap.c
+++ keyboards/beanboards/reform/keymaps/default/keymap.c
@@ -48,12 +48,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          * | MENU | LALT | LGUI | LCTL | LOWER| RSFT | PIPE |      |   \  | SPACE| RAISE| LEFT | DOWN |  UP  | RIGHT|
          * `-------------------------------------------------------+------------------------------------------------'
         */
-        KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_PMNS,    KC_P1,      KC_PPLS,    KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_LPRN,    KC_P2,      KC_RPRN,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
-        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LCBR,    KC_P3,      KC_RCBR,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
-        KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_LBRC,    KC_P4,      KC_RBRC,    KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
-        KC_CAPS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LT,      KC_P5,      KC_GT,      KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,
-        KC_P9,    KC_LALT,    KC_LGUI,    KC_LCTL,    KC_P7,      KC_RSFT,    KC_PIPE,    KC_P6,      KC_SLSH,    KC_SPC,     KC_P8,      KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT
+        KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_PMNS,    KC_NO,      KC_PPLS,    KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
+        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_LPRN,    KC_NO,      KC_RPRN,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
+        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LCBR,    KC_NO,      KC_RCBR,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
+        KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_LBRC,    KC_NO,      KC_RBRC,    KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
+        KC_CAPS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LT,      KC_NO,      KC_GT,      KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,
+        RO_MENU,    KC_LALT,    KC_LGUI,    KC_LCTL,    _LOWER,     KC_RSFT,    KC_PIPE,    KC_NO,      KC_SLSH,    KC_SPC,     _RAISE,     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT
     ),
     [_LOWER] = LAYOUT_ortho(
         /*
